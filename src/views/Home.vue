aa9d3464da74e19fe42e429dce60088d5523da23
Nadia Kaloyanova <nadia_i_k@yahoo.com>
2019-08-06 22:44:52 +0300

    Updating the home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <HelloWorld />
+    <v-container>
+        <v-layout text-center wrap>
+            <v-flex xs12>
+                <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
+            </v-flex>
+
+            <v-flex mb-4>
+                <h1 class="display-2 font-weight-bold mb-3">Healthy Living</h1>
+                <p class="subheading font-weight-regular">
+                    Track your food intake and sport activity for a healthy life.
+                </p>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+      components: {
+      }
+  };
 </script>
