c36fb1ba150245abf53cd02ba729c1d3db42143d
Nadia Kaloyanova <nadia_i_k@yahoo.com>
2019-08-06 22:30:21 +0300

    Setting up the layout.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,75 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-app>
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense clipped>
+                <v-list-item to="/">
+                    <v-list-item-action>
+                        <v-icon>mdi-star</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item to="/sign-in">
+                    <v-list-item-action>
+                        <v-icon>mdi-star</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Sign In</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="text-uppercase">
+                <span>Healthy Living</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <!-- <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <span class="mr-2">Latest Release</span>
+            </v-btn> -->
+        </v-app-bar>
+
+        <v-content>
+            <router-view />
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import { AppConfig, UserSession } from "blockstack";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+    name: "App",
+
+    components: {
+    },
+
+    created() {
+        const appConfig = new AppConfig(["store_write", "publish_data"]);
+        const session = new UserSession(appConfig);
+
+        this.setSession(session);
+
+        if (session.isUserSignedIn()) {
+            const userData = session.loadUserData();
+            const username = userData.username;
+
+            this.setUsername(username);
+        }
+        else if (session.isSignInPending()) {
+            session.handlePendingSignIn().then(userData => {
+                window.location = window.location.origin;
+            });
+        }
+    },
+
+    data: () => ({
+        drawer: null
+    })
 };
 </script>
