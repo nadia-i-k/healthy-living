dbad4e3d1deca97ac074e00341c3bc2537ebd5d4
Nadia Kaloyanova <nadia_i_k@yahoo.com>
2019-08-07 16:08:26 +0300

    Adding UC message.

--- src/views/Sport.vue
+++ src/views/Sport.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Sport</h1>
+  <v-container>
+        <v-layout>
+            <v-flex xs12 md6 offset-md3>
+                <h1>Sport Activities</h1>
+                <p>Under construction.</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
