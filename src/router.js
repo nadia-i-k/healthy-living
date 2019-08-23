35c14be0e0c48dead8827fda14322ea18b7c1592
Nadia Kaloyanova <nadia_i_k@yahoo.com>
2019-08-23 21:19:51 +0300

    Implementing authentication guard.

--- src/router.js
+++ src/router.js
@@ -1,30 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store';
 import Home from './views/Home.vue'
 import Food from './views/Food.vue'
 import Sport from './views/Sport.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  hash: false,
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/food',
-      name: 'food',
-      component: Food
-    },
-    {
-      path: '/sport',
-      name: 'sport',
-      component: Sport
-    },
-  ]
+const router = new Router({
+    mode: 'history',
+    hash: false,
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+            meta: {
+                protected: false
+            }
+        },
+        {
+            path: '/food',
+            name: 'food',
+            component: Food,
+            meta: {
+                protected: true
+            }
+        },
+        {
+            path: '/sport',
+            name: 'sport',
+            component: Sport,
+            meta: {
+                protected: true
+            }
+        },
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    const isProtected = to.meta.protected || false;
+
+    if (isProtected) {
+        if (store.state.username) {
+            next();
+        }
+        else {
+            next('/sign-in');
+        }
+    }
+    else {
+        next();
+    }
 })
+
+export default router;
